9ffed35306f5fdb6f71b622db17599ca3e7b780f
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-31 18:28:39 -0500

    Add person class

--- Person.rb
+++ Person.rb
@@ -1,3 +1,31 @@
 class Person
+    attr_reader :id
+    attr_accessor :name, :age
 
+    @@counter = 0
+
+    def initialize(age, name='unknown', parent_permission=true)
+      @@counter += 1
+      @id = @@counter
+      @name= name
+      @age = age
+      @parent_permission = parent_permission
+    end
+
+    private
+    def is_of_age?
+      if @age >= 18
+        true
+      else
+        false
+      end
+    end
+
+    public
+    def can_use_services?
+      is_of_age? || @parent_permission
+    end
 end
+
+person = Person.new(11, 'Daniel', true)
+p person.can_use_services?
