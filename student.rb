456e2e7dd13686103f20675d65ac043fb3d336cb
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-08 09:57:44 -0500

    Add student

--- student.rb
+++ student.rb
@@ -1,26 +1,14 @@
-class Person
-    attr_reader :id
-    attr_accessor :name, :age
-  
-    @counter = 0
-  
-    def initialize(age, name = 'unknown', parent_permission: true)
-      @counter += 1
-      @id = @@counter
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    public
-  
-    def can_use_services?
-      is_of_age? || @parent_permission
-    end
+require './person'
+
+class Student < Person
+  attr_accessor :specialization
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: 'Unknown')
+    super(age, name, parent_permission)
+    @classroom = classroom
   end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
