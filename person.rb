fd536dba4088543ebae0e906e2406d41dda76da0
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-08 09:59:13 -0500

    fix linters

--- person.rb
+++ person.rb
@@ -1,26 +1,26 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-  
-    @counter = 0
-  
-    def initialize(age, name = 'unknown', parent_permission: true)
-      @counter += 1
-      @id = @@counter
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    public
-  
-    def can_use_services?
-      is_of_age? || @parent_permission
-    end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  @counter = 0
+
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @counter += 1
+    @id = @@counter
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+end
