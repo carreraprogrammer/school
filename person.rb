a7379ddd9ec9019cef5d1c1d14c1f657998f338f
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-09 14:35:28 -0500

    Fix linters

--- person.rb
+++ person.rb
@@ -34,18 +34,3 @@ class Person < Nameable
     @name
   end
 end
-
-person = Person.new(22, 'maximilianus')
-p person.correct_name
-
-#=> "maximilianus"
-
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-
-#=> "MAXIMILIANUS"
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
-
-# => "MAXIMILIAN"
