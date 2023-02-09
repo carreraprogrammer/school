724e28c37243b87d3f6ee9510c43ff379fe52abd
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-09 10:33:30 -0500

    Add nameable_object as an instance variable from Person class

--- person.rb
+++ person.rb
@@ -1,17 +1,19 @@
-require './nameable.rb'
+require './nameable'
 
 class Person < Nameable
+
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :nameable_object
 
   @counter = 0
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true, nameable_object: nil)
     @counter += 1
     @id = @@counter
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @nameable_object = nameable_object
   end
 
   private
