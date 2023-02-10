aa5775962f589486c730939c04432e44dbfd4751
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-10 11:27:27 -0500

    Finalize has-many side relation

--- student.rb
+++ student.rb
@@ -5,9 +5,10 @@ class Student < Person
   attr_accessor :specialization, :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: 'Unknown')
+    raise "The class doesn't exist, please create an object classroom first" unless classroom.is_a?(Classroom)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    @@classroom.students << self
+    @classroom.students << self
   end
 
   def play_hooky
@@ -18,5 +19,8 @@ end
 
 math_class = Classroom.new("Math")
 
-
+p "This is the class"
 p math_class
+
+student1 = Student.new(math_class, 16)
+p student1
