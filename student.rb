5f0016f807b8875df1f28836db72523a633d95f5
carreraprogrammer <carreraprogrammer@gmail.com>
2023-02-10 10:03:33 -0500

    Add students to the class ussing << self

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: 'Unknown')
     super(age, name, parent_permission)
     @classroom = classroom
+    @classroom.students << self
   end
 
   def play_hooky
